314baa824ae599db3d67477b740cdc8a48879798
Tini Gisa <1814759@nzse.ac.nz>
2021-06-08 22:52:25 +1200

    Add project files.

---
+++ PositiveOrNegative/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PositiveOrNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+
+            Console.WriteLine("enter only number1");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+           if (number1 > -0)
+            {
+                Console.WriteLine("positive");
+            }
+           if(number1 < 0)
+            {
+                Console.WriteLine("negative");
+            }
+        }
+    }
+}
